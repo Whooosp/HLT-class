a4d7a450231e1fa80d2b2ccb72a686b18652cb7c
Whooosp <vss180000@utdallas.edu>
2022-10-08 21:00:12 -0500

    Finished web crawler program

--- Web Crawling/vss180000_main.py
+++ Web Crawling/vss180000_main.py
@@ -1,6 +1,13 @@
+import os
+
 import requests
 import re
 from bs4 import BeautifulSoup
+import string
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+import pandas as pd
 
 
 def visible(element):
@@ -14,6 +21,9 @@ def visible(element):
 def scrape_url(url: str, to_save: bool):
     r = requests.get(url)
 
+    if r.status_code >= 400:
+        return
+
     data = r.text
     soup = BeautifulSoup(data, 'html.parser')
 
@@ -21,6 +31,8 @@ def scrape_url(url: str, to_save: bool):
 
     titles = [t for t in data if t.parent.name == "title"]
     title = titles[0] if len(titles) > 0 else url[url.index('://')+3:].replace('/', '_')
+    title = ''.join(c for c in title if c.isalnum())
+    title = f'url_{title}'
     print(title)
 
     if to_save:
@@ -39,21 +51,59 @@ if __name__ == "__main__":
 
     for link in soup.find_all('a'):
         link_str = str(link.get('href'))
-        if ('Carlsen' in link_str or 'carlsen' in link_str) and 'chessbomb' not in link_str:
+        if ('Carlsen' in link_str or 'carlsen' in link_str) and 'chessbomb' not in link_str and '.no/' not in link_str:
             if link_str.startswith('/url?q='):
                 link_str = link_str[7:]
             if '&' in link_str:
                 i = link_str.find('&')
                 link_str = link_str[:i]
-            if link_str.startswith('http') and 'google' not in link_str:
-                urls.add(link_str)
+            if link_str.startswith('http') and 'google' not in link_str and link_str not in urls:
+                print(link_str)
+                if scrape_url(link_str, True):
+                    urls.add(link_str)
 
         if len(urls) > 14:
             break
 
-    for url in urls:
-        print(url)
-        scrape_url(url, True)
-    # for soup in
+    all_sentences = set()
+    for filename in [name for name in os.listdir(os.getcwd()) if name[:3] == "url"]:
+        save_string = ''
+        with open(filename, 'r', encoding='utf8') as f:
+            for line in f:
+                temp_line = ''.join(filter(lambda x: x in set(string.printable), ' '.join(line.split())))
+                temp_line = temp_line + '.' if temp_line != '' and temp_line[-1] not in '.!?' else temp_line
+                save_string += temp_line + ' ' if temp_line.strip() != '' else temp_line
+        sentences = sent_tokenize(save_string)
+        all_sentences = all_sentences | set(sentences)
+        with open("better"+filename, 'w', encoding='utf8') as f:
+            f.write(' '.join(sentences))
+
+    token_counts = Counter()
+    for filename in [name for name in os.listdir(os.getcwd()) if name.startswith("betterurl")]:
+
+        with open(filename, 'r', encoding='utf8') as f:
+            tokens = [t.lower() for t in word_tokenize(f.read()) if t.lower() not in stopwords.words('english')
+                      and t.isalpha()]
+            token_counts += Counter(tokens)
+    for word, count in sorted(token_counts.items(), key=lambda item: item[1], reverse=True)[:25]:
+        print(f'Word: {word}, Count: {count}')
+    # Chosen words
+    words = ['carlsen', 'chess', 'tournament', 'wins', 'championship', 'rapid', 'blitz', 'classical', 'win', 'world']
+
+    df = pd.DataFrame(columns=words, index=list(all_sentences))
+
+    for sentence in all_sentences:
+        df.loc[sentence] = [True if word in sentence else False for word in words]
+    df = df[df.any(axis=1)]
+
+    print(df.head())
+
+    # DF is our knowledge base, stored efficiently such that each sentence is a row in a table
+    # where each word has a True value in that row iff the word exists in the sentence. Therefore
+    # when someone wants to know more about a word, all we need to do is return the sentences
+    # that have a value of True in that column
+
+    df.to_pickle('knowledge_base.p')
+
     # end of program
     print("end of crawler")
